64b3a0f3b41c703044dd03079194141f3d8e351a
Laura Alejandra Izquierdo Castro <alizeci17@gmail.com>
2021-09-29 14:51:35 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Construcción de Aplicación Web Segura
+
+## Autor ![Autor](https://img.icons8.com/fluency/30/000000/person-female.png)
+Laura Alejandra Izquierdo Castro
---
+++ src/main/java/co/edu/escuelaing/arep/HelloService.java
@@ -0,0 +1,27 @@
+package co.edu.escuelaing.arep;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.secure;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloService {
+	public static void main( String[] args )
+    {
+        port(getPort());
+        
+        //API: secure(keystoreFilePath, keystorePassword, truststoreFilePath, truststorePassword);
+        secure("keystores/ecikeystore.p12", "123456", null, null);
+        get("hello", (req,res) -> "Hello World, here goes your security!");
+    }
+
+	private static int getPort() {
+		if (System.getenv("PORT") != null) {
+			return Integer.parseInt(System.getenv("PORT"));
+		}
+		return 4567;
+	}
+}
